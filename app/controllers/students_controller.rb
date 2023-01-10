ec9ffbf5abd6849d5285f09831918e32ec02e2e9
David Tewkesbury <tewkesburyd@hotmail.com>
2023-01-10 10:39:03 -0700

    commit tree e18d684cb934ed99ef232af87affeaf91c5c2c82

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,14 @@
 class StudentsController < ApplicationController
 
   def index
+    params = {first_name: params[:first_name], last_name: params[:last_name]}
     students = Student.all
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
